e74f26d438cef1eb50ede44516c58383a6561d7d
Ildar Khabatulin <ildar.khabatulin@gmail.com>
2020-06-23 21:11:12 +0200

    Add support for line breaks for encoded and decoded strings
    I already regret adding this

--- decoder/src/MorseDecoder.cpp
+++ decoder/src/MorseDecoder.cpp
@@ -13,15 +13,29 @@ Result<std::string> MorseDecoder::encode(std::string_view input) const
     std::string encodedString;
     for (const auto& symbol : input)
     {
-        auto encodingSymbolResult = m_dict->encode(symbol);
-        if (encodingSymbolResult.status == Status::Error)
+        if (symbol == '\n' || symbol == '\r')
         {
-            return Result<std::string>::makeError(encodingSymbolResult.errorMsg);
+            if (encodedString.back() == ' ')
+            {
+                encodedString.pop_back();
+            }
+            encodedString.append(1, symbol);
         }
-        encodedString.append(encodingSymbolResult.value);
-        encodedString.append(1, ' ');
+        else
+        {
+            auto encodingSymbolResult = m_dict->encode(symbol);
+            if (encodingSymbolResult.status == Status::Error)
+            {
+                return Result<std::string>::makeError(encodingSymbolResult.errorMsg);
+            }
+            encodedString.append(encodingSymbolResult.value);
+            encodedString.append(1, ' ');
+        }
     }
-    encodedString.pop_back(); // easier to remove last space than rewriting the loop
+    if (encodedString.back() == ' ')
+    {
+        encodedString.pop_back(); // easier to remove last space than rewriting the loop
+    }
     return Result<std::string>::makeSuccess(encodedString);
 }
 
@@ -29,17 +43,34 @@ Result<std::string> MorseDecoder::decode(std::string_view input) const
 {
     std::string decodedString;
 
-    size_t spacePosition = 0;
-    while (spacePosition != std::string_view::npos)
+    size_t delimiterPosition = 0;
+    while (delimiterPosition != std::string_view::npos)
     {
-        spacePosition = input.find(' ');
-        auto decodingSymbolResult = m_dict->decode(input.substr(0, spacePosition));
+        delimiterPosition = input.find_first_of(" \n");
+        auto chunk = input.substr(0, delimiterPosition);
+        if (chunk.empty())
+        {
+            break;
+        }
+
+        std::string suffix;
+        if (input[delimiterPosition] == '\n')
+        {
+            if (chunk.back() == '\r')
+            {
+                chunk.remove_suffix(1);
+                suffix.push_back('\r');
+            }
+            suffix.push_back('\n');
+        }
+        auto decodingSymbolResult = m_dict->decode(chunk);
         if (decodingSymbolResult.status == Status::Error)
         {
             return Result<std::string>::makeError(decodingSymbolResult.errorMsg);
         }
         decodedString.append(1, decodingSymbolResult.value);
-        input.remove_prefix(spacePosition + 1);
+        decodedString.append(suffix);
+        input.remove_prefix(delimiterPosition + 1);
     }
 
     return Result<std::string>::makeSuccess(decodedString);
